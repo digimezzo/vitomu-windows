0eab803f7e0d7e0c8085feb2c6201f1b706ced52
Raphaël <raphael.godart@outlook.com>
2017-07-03 16:21:13 +0200

    Ctrl+L opens log file directory

--- Vitomu/Shell.xaml.cs
+++ Vitomu/Shell.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿using Digimezzo.Utilities.IO;
+using Digimezzo.Utilities.Log;
+using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Vitomu.Controls;
 using Vitomu.Views;
 
@@ -20,6 +24,21 @@ namespace Vitomu
             this.ShellFrame.Navigate(new Main());
             this.ShowWindowControls = true;
         }
-        #endregion  
+        #endregion
+
+        private void VitomuWindow_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            if (Keyboard.Modifiers == ModifierKeys.Control & e.Key == Key.L)
+            {
+                try
+                {
+                    Actions.TryViewInExplorer(LogClient.Logfile()); // View the log file
+                }
+                catch (Exception ex)
+                {
+                    LogClient.Error("Could not view the log file {0} in explorer. Exception: {1}", LogClient.Logfile(), ex.Message);
+                }
+            }
+        }
     }
 }
